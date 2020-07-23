6d36356d3ba6e42e1f8520fa8264dd7db7fad80e
arn-the-long-beard <arn.menant@gmail.com>
2020-07-23 16:11:28 +0200

    feat: added basic impl for game of life

--- README.md
+++ README.md
@@ -1,66 +1,32 @@
-# Seed Quickstart
+# Seeded Game of life 
 
-> Basic Rust-only template for your new Seed app.
+> This is a rewrite of the original game of life from 
+> Original project from  : https://github.com/rustwasm/wasm_game_of_life
 
-## 1. Create a new project
 
-1. The simplest way how to do it is to click on the green button **Use this template** on the GitHub [profile](https://github.com/seed-rs/seed-quickstart) of this quickstart.
+### Motivation
 
-1. Make sure Git doesn't automatically convert your newlines to CLRF because linters don't like it.
-    - Run `$ git config --global core.autocrlf` in your terminal and it should return `input` or `false`. See [Git docs](https://git-scm.com/book/en/v2/Customizing-Git-Git-Configuration) for more info.
+Well, I followed the very nice tutorial from there https://rustwasm.github.io/docs/book/introduction.html .
+And it looks so awesome to use Rust for doing web stuff that I got more Curious. As a redux/ngrx/angular guy, I know many of the challenge of javascript/typescript.
+So I was like , omg Rust is so OP , if we can use it for front end, then development, maintenance, team skill growth would be just so disruptive.
+Then I found seed and I was like >< so excited. After testing other alternative, 
+I found that seed is the one that use the most the full potential of Rust and it is the easier to start new project with.
 
-1. Clone your new repository to your local machine. I use [GitKraken](https://www.gitkraken.com/), but you are probably better developer than me - use your favorite terminal.
 
-## 2. Install / check required tools
+### 1. Clone the project
 
-1. Make sure you have basic tools installed:
+    git clone https://github.com/arn-the-long-beard/seeded_game_of_life
 
-   - [Rust](https://www.rust-lang.org) 
-     - Check: `$ rustc -V` => `rustc 1.43.1 (8d69840ab 2020-05-04)`
-     - Install: https://www.rust-lang.org/tools/install
-   - [cargo-make](https://sagiegurari.github.io/cargo-make/)
-     - Check: `$ cargo make -V` => `cargo-make 0.30.7`
-     - Install: `$ cargo install cargo-make`
-       
-1. Platform-specific tools like `ssl` and `pkg-config`:
-    - Follow recommendations in build errors (during the next chapter).
-    - _Note_: Don't hesitate to write notes or a tutorial for your platform and create a PR .
+### 2. Run the project
 
-## 3. Prepare your project for work
+    cargo make watch
+ 
+    cargo make server
+  
+  
+### NB 
 
-1. Open the project in your favorite IDE (I recommend [VS Code](https://code.visualstudio.com/) + [Rust Analyzer](https://rust-analyzer.github.io/)).
-1. Open a new terminal tab / window and run: `cargo make serve`
-1. Open a second terminal tab and run: `cargo make watch`
-1. If you see errors, try to fix them or write on our [chat](https://discord.gg/JHHcHp5) or [forum](https://seed.discourse.group/).
-1. Modify files like `README.md` and `Cargo.toml` as you wish.
-
-## 4. Write your website
-
-1. Open [localhost:8000](http://localhost:8000) in a browser (I recommend Firefox and Chrome).
-1. Modify source files (e.g. `/src/lib.rs` or `/index.html`).
-1. Watch compilation in the terminal tab where you run `cargo make watch`.
-1. You can watch dev-server responses in the tab where you run `cargo make serve`.
-1. Refresh your browser and see changes.
-1. Go to step 2.
-
-## 5. Prepare your project for deploy
-
-1. Run `cargo make verify` in your terminal to format and lint the code.
-1. Run `cargo make build_release`.
-1. Upload `index.html` and `pkg` into your server's public folder.
-   - Don't forget to upload also configuration files for your hosting, see the [Netlify](https://www.netlify.com/) one below.
-
-```toml
-# netlify.toml
-[[redirects]]
-  from = "/*"
-  to = "/index.html"
-  status = 200
-```
-
-## Other Seed quickstarts and projects
-
-- [seed-rs/awesome-seed-rs](https://github.com/seed-rs/awesome-seed-rs)
+There is no js dependency on my project. It is pure Rust :)   
 
 ---
 
--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,10 @@
 // but some rules are too "annoying" or are not applicable for your case.)
 #![allow(clippy::wildcard_imports)]
 
+mod universe;
+mod utils;
+
+use crate::universe::{Cell, Universe};
 use seed::{prelude::*, *};
 
 // ------ ------
@@ -10,8 +14,19 @@ use seed::{prelude::*, *};
 // ------ ------
 
 // `init` describes what should happen when your app started.
-fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
-    Model::default()
+fn init(_: Url, orders: &mut impl Orders<Msg>) -> Model {
+    orders.after_next_render(|_| Msg::PrepareCanvas);
+
+    Model {
+        cell_size: 5,
+        grid_color: "#CCCCCC".to_string(),
+        dead_color: "#FFFFFF".to_string(),
+        alive_color: "#000000".to_string(),
+        pause: false,
+        universe: Universe::new(),
+        canvas_height: 0,
+        canvas_width: 0,
+    }
 }
 
 // ------ ------
@@ -19,8 +34,16 @@ fn init(_: Url, _: &mut impl Orders<Msg>) -> Model {
 // ------ ------
 
 // `Model` describes our app state.
-type Model = i32;
-
+pub struct Model {
+    cell_size: u32,
+    grid_color: String,
+    dead_color: String,
+    alive_color: String,
+    universe: Universe,
+    pause: bool,
+    canvas_height: u32,
+    canvas_width: u32,
+}
 // ------ ------
 //    Update
 // ------ ------
@@ -29,14 +52,107 @@ type Model = i32;
 #[derive(Copy, Clone)]
 // `Msg` describes the different events you can modify state with.
 enum Msg {
-    Increment,
+    PrepareCanvas,
+    Play,
+    Pause,
+    Draw,
 }
 
 // `update` describes how to handle each `Msg`.
-fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
+fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
     match msg {
-        Msg::Increment => *model += 1,
+        Msg::PrepareCanvas => {
+            model.canvas_width = (model.cell_size + 1) * model.universe.width() + 1;
+            model.canvas_height = (model.cell_size + 1) * model.universe.height() + 1;
+            orders.after_next_render(|_| Msg::Draw);
+        }
+        Msg::Draw => {
+            if model.pause {
+            } else {
+                model.universe.tick();
+                draw_grid(model);
+                draw_cells(model);
+                orders.after_next_render(|_| Msg::Draw);
+            }
+        }
+        Msg::Play => {
+            model.pause = false;
+            orders.after_next_render(|_| Msg::Draw);
+        }
+        Msg::Pause => model.pause = true,
     }
+}
+
+fn draw_grid(model: &mut Model) {
+    let canvas = canvas("game-of-life-canvas").unwrap();
+    canvas.set_width(model.canvas_width);
+    canvas.set_height(model.canvas_height);
+    let ctx = seed::canvas_context_2d(&canvas);
+    ctx.begin_path();
+    ctx.set_stroke_style(&JsValue::from_str(model.grid_color.as_str()));
+
+    // Vertical lines.
+    for i in 0..model.universe.width() {
+        ctx.move_to((i * (model.cell_size + 1) + 1).into(), 0.);
+        ctx.line_to(
+            (i * (model.cell_size + 1) + 1).into(),
+            ((model.cell_size + 1) * model.universe.height() + 1).into(),
+        );
+    }
+    // Horizontal lines.
+    for j in 0..model.universe.height() {
+        ctx.move_to(0., (j * (model.cell_size + 1) + 1).into());
+        ctx.line_to(
+            ((model.cell_size + 1) * model.universe.width() + 1).into(),
+            (j * (model.cell_size + 1) + 1).into(),
+        )
+    }
+
+    ctx.stroke();
+}
+
+fn draw_cells(model: &mut Model) {
+    let canvas = canvas("game-of-life-canvas").unwrap();
+    let ctx = seed::canvas_context_2d(&canvas);
+    ctx.begin_path();
+
+    // Alive cells.
+    ctx.set_fill_style(&JsValue::from_str(model.alive_color.as_str()));
+    for row in 0..model.universe.height() {
+        for col in 0..model.universe.width() {
+            let idx = model.universe.get_index(row, col);
+            if model.universe.cell_at_index(idx) != Cell::Alive {
+                continue;
+            }
+
+            ctx.fill_rect(
+                (col * (model.cell_size + 1) + 1).into(),
+                (row * (model.cell_size + 1) + 1).into(),
+                (model.cell_size).into(),
+                (model.cell_size).into(),
+            );
+        }
+    }
+
+    // Dead cells.
+    ctx.set_fill_style(&JsValue::from_str(model.dead_color.as_str()));
+    for row in 0..model.universe.height() {
+        for col in 0..model.universe.width() {
+            let idx = model.universe.get_index(row, col);
+            if model.universe.cell_at_index(idx) != Cell::Dead {
+                continue;
+            }
+
+            ctx.fill_rect(
+                (col * (model.cell_size + 1) + 1).into(),
+                (row * (model.cell_size + 1) + 1).into(),
+                (model.cell_size).into(),
+                (model.cell_size).into(),
+            );
+        }
+    }
+
+    ctx.stroke();
 }
 
 // ------ ------
@@ -47,14 +163,24 @@ fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
 #[allow(clippy::trivially_copy_pass_by_ref)]
 // `view` describes what to display.
 fn view(model: &Model) -> Node<Msg> {
-    div![
-        "This is a counter: ",
-        C!["counter"],
-        button![model, ev(Ev::Click, |_| Msg::Increment),],
-    ]
+    if model.canvas_width == 0 && model.canvas_height == 0 {
+        div!["Loading canvas"]
+    } else {
+        section![
+            button![
+                id!("play-pause"),
+                if model.pause {
+                    ev(Ev::Click, |_| Msg::Play)
+                } else {
+                    ev(Ev::Click, |_| Msg::Pause)
+                },
+                if model.pause { "⏸" } else { "▶" }
+            ],
+            canvas![id!("game-of-life-canvas")],
+        ]
+    }
 }
 
-// ------ ------
 //     Start
 // ------ ------
 

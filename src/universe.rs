76fbcf3384be6f5022e99d2154fda51411319773
arn-the-long-beard <arn.menant@gmail.com>
2020-07-27 12:35:55 +0200

    fix: removed import & macro

--- src/universe.rs
+++ src/universe.rs
@@ -1,14 +1,6 @@
 use crate::utils;
 use rand_core::{OsRng, RngCore};
 use std::fmt;
-use web_sys::console;
-
-// A macro to provide `println!(..)`-style syntax for `console.log` logging.
-macro_rules! log {
-    ( $( $t:tt )* ) => {
-        web_sys::console::log_1(&format!( $( $t )* ).into());
-    }
-}
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
 #[cfg(feature = "wee_alloc")]
